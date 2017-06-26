73584a46c1a4fb50694550d39a79b7e3f9fb9978
clarson469 <clarson469@live.com>
2017-06-26 14:06:46 +0100

    added makedir lines for log and weight folders

--- settings.py
+++ settings.py
@@ -3,9 +3,14 @@ import sys, os
 # sys info
 BASE_DIR = os.path.realpath(os.path.dirname(sys.argv[0]))
 WEIGHT_DIR = os.path.join(BASE_DIR, 'data\\weights')
+if not os.path.exists(WEIGHT_DIR):
+    os.makedirs(WEIGHT_DIR)
+    
 MNIST_DIR = os.path.join(BASE_DIR, 'data\\MNIST')
 STATS_DIR = os.path.join(BASE_DIR, 'data\\stats')
 LOG_DIR = os.path.join(BASE_DIR, 'log')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
 # model hyperparameters
 iterations = 200
